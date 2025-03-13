952ecc58460bb3e40e633c8c5fb9a13fc836daff
Venkataramana Kopalli <136314523+KopalliV@users.noreply.github.com>
2025-03-13 13:50:31 +0100

    Update 06_compare_results.sh

--- read_mapping/06_compare_results.sh
+++ read_mapping/06_compare_results.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 # Step 6: Compare mappings and compute cumulative rates
 
+##Giraffe
+# Annotate giraffe mappings for comparison
+vg annotate -a giraffe_sim_mappings.gam -m -x merged_graph.giraffe.gbz > giraffe_annotated.gam
+
+# Compare mappings using vg gamcompare
+vg gamcompare -s -r 100 -T giraffe_annotated.gam simulated_reads.gam > giraffe_scores.tsv
+
+# Generate cumulative alignment and correctness rates
+echo -e "min_mapQ\treads_aligned\treads_aligned_correctly" > giraffe_cumulative_rates.tsv
+awk 'NR>1 {aligned[$2]++} NR>1&&$1==1{correct[$2]++}
+     END {OFS="\t"; for (i in aligned) print i, aligned[i], correct[i]}' giraffe_scores.tsv > temp_aligned_giraffe_correct.tsv
+
+# Sort results by minimum mapping quality in descending order
+sort -nrk1,1 temp_aligned_giraffe_correct.tsv > sorted_alignment_giraffe_rates.tsv
+
+# Compute cumulative statistics
+wk '{OFS="\t"; cum_aligned+=$2; cum_correct+=$3; print $1, cum_aligned, cum_correct}' sorted_alignment_giraffe_rates.tsv >> giraffe_cumulative_rates.tsv
+
+##Minimap2
+
 # Annotate Minimap2 mappings for comparison
 vg annotate -a minimap_mappings.gam -m -x updated_graph.gbz > annotated_minimap.gam
 
